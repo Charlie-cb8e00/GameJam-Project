f4388ddfcef6711898257939ef836a235f6af956
Charlie-cb8e00 <charliehowe575@gmail.com>
2026-01-31 09:35:16 +0100

    player movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,28 @@
+
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public InputActionReference inputAction;
+    public int moveSpeed = 500;
+    public Rigidbody player;
+
+
+    void Start()
+    {
+        inputAction.action.Enable();
+
+    }
+
+
+    void Update()
+    {
+
+        Vector2 movement = inputAction.action.ReadValue<Vector2>() * moveSpeed;
+        player.AddForce(movement.x, 0, movement.y);
+
+    }
+
+}
+
