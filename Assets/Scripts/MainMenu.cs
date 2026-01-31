aa7e7476bb8f941517779cba79264c7de97b704c
lauramun23 <lauramunoz.23@campuscamara.es>
2026-01-31 19:36:50 +0100

    update

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -7,20 +7,21 @@ public class MainMenu : MonoBehaviour
 {
     public void Jugar()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene("Computer");
     }
 
     public void Tutorial()
     {
-
+        SceneManager.LoadScene("Tutorial");
     }
 
     public void Salir()
     {
         Application.Quit();
+        Debug.Log("Has salido del juego");
     }
     public void Creditos()
     {
-
+        SceneManager.LoadScene("Creditos");
     }
 }
