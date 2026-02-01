d2ffe4e910374b8005c29da3916d45e3daccc30e
ks-ecko <alangabrielveronmiranda5.0@gmail.com>
2026-02-01 11:22:16 +0100

    Hoja de interes con emission

---
+++ Assets/Scripts/UI/Menu/CreditosMenu.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CreditosMenu : MonoBehaviour
+{
+    public AudioSource buttonSound;
+    public float delay = 0.3f;
+
+    public void Atras()
+    {
+        StartCoroutine(LoadSceneDelay("Menu"));
+    }
+
+
+    IEnumerator LoadSceneDelay(string sceneName)
+    {
+        buttonSound.Play();
+        yield return new WaitForSeconds(delay);
+        SceneManager.LoadScene(sceneName);
+    }
+}
